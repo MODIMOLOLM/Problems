8c98310e1f95ade4cda5648017ba143429069d6d
Modimo Максим <modimololm@ukr.net>
2020-03-12 17:47:48 +0200

    Task3 done, task 2 fixed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -16,7 +16,7 @@ namespace Task2
             int min3 = int.MaxValue;
 
             int j;
-            int[] a = { 1,5,4,2};
+            int[] a = { 1,5,4,2, 1, 5, 4, 2 , 4, 31, 1, 1};
 
             for (int i=0;i<a.Length;i++)
             {
@@ -34,12 +34,43 @@ namespace Task2
             }
             for (int i = 0; i < a.Length; i++)
             {
-                if (min2 > a[i] && min1 != a[i] && min2 != a[i]) min2 = a[i];
+                if (min3 > a[i] && min1 != a[i] && min2 != a[i]) min3 = a[i];
             }
 
             Console.WriteLine("1 " + min1);
             Console.WriteLine("2 " + min2);
             Console.WriteLine("3 " + min3);
+
+            min1 = int.MaxValue;
+            min2 = int.MaxValue;
+            min3 = int.MaxValue;
+
+            int min = int.MaxValue;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (min1 > a[i])
+                {
+                    min1 = i;
+                    min = a[i];
+                }
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (min2 > a[i] && min1 != i) min2 =i;
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (min2 > a[i] && min1 != i && min2 != i) min3=i;
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("1 " + min1);
+            Console.WriteLine("2 " + min2);
+            Console.WriteLine("3 " + min3);
+            Console.WriteLine("min = " + min);
+
 
         }
     }
