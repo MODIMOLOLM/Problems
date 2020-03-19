26200d817d1f19ec0f457e9c330e1ec5f6bc3129
Modimo Максим <modimololm@ukr.net>
2020-03-19 16:45:18 +0200

    IsPangram reload

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -10,6 +10,27 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+
         }
+        public static bool IsPangram(string str)
+        {
+            string str1=str.ToLower();
+            int a = 0;
+            char[] alphabet = new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (str1[i] == alphabet[j])
+                    {
+                        a++;
+                        alphabet[j] = '0';
+                    }
+                }
+            }
+            if (a == 26) return true;
+            else return false;
+        }
+
     }
 }
